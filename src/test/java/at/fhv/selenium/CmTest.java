883924a2eb8b82348611ba6860cb23660969f3ed
Simon <sim.keckeis@gmail.com>
2020-01-31 16:37:38 +0100

    selenium firefox

--- src/test/java/at/fhv/selenium/CmTest.java
+++ src/test/java/at/fhv/selenium/CmTest.java
@@ -32,12 +32,12 @@ public class CmTest {
 //      /usr/bin/google-chrome-stable
 //      System.setProperty("webdriver.chrome.driver","C:\\Users\\Computer\\Desktop\\chromedriver.exe");
 //      System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome-stable");
-      ChromeOptions options = new ChromeOptions();
-      options.addArguments("--headless");
-      options.addArguments("--no-sandbox");
-      options.addArguments("--disable-dev-shm-usage");
-      options.setBinary("/usr/bin/google-chrome-stable");
-    driver = new ChromeDriver();
+//      ChromeOptions options = new ChromeOptions();
+//      options.addArguments("--headless");
+//      options.addArguments("--no-sandbox");
+//      options.addArguments("--disable-dev-shm-usage");
+//      options.setBinary("/usr/bin/google-chrome-stable");
+    driver = new FirefoxDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
   }
